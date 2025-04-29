1d5168d8cabd3ae27942cb9faf6a89d3aa8104c6
RomaZinkevich <zra1903@mail.ru>
2025-04-29 12:54:04 +0300

    init commit

---
+++ src/main/java/com/zirom/tutorapi/TutorapiApplication.java
@@ -0,0 +1,13 @@
+package com.zirom.tutorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zirom/tutorapi/TutorapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zirom.tutorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
